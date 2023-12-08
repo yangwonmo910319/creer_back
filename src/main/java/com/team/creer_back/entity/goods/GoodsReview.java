61eb0a66966e8e28d7213ce0900b025af16022e6
yam <yangwm0319@naver.com>
2023-12-08 17:30:01 +0900

    12/08 원모

--- src/main/java/com/team/creer_back/entity/goods/GoodsReview.java
+++ src/main/java/com/team/creer_back/entity/goods/GoodsReview.java
@@ -20,26 +20,19 @@ public class GoodsReview {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long goodsReviewId;
-
     @ManyToOne(fetch = FetchType.LAZY)  // 물품 번호로 리뷰 찾기
     @JoinColumn(name = "goods_Detail_id")
     private GoodsDetail goodsDetail;
-
     @ManyToOne(fetch = FetchType.LAZY)  // 유저 닉네임, 프사
     @JoinColumn(name = "member_id")
     private Member member;
-
     private LocalDateTime reviewDate;   // 날짜 넣어줌
     @PrePersist
     public void perPersist() {
         reviewDate = LocalDateTime.now();
     }
-
-    private String reviewStar;          // 별점
-
+    private double reviewRating;          // 별점
     private String reviewImg;           // 리뷰 이미지
     @Column(length = 1000)
     private String reviewContent;       // 리뷰 글
-
-
 }
