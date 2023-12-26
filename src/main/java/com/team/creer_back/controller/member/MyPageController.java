58ce520468b2d24b60b64ef333d95ccb921927d8
ydj1215 <ydjdj2@naver.com>
2023-12-26 09:57:52 +0900

    231226 ydj 0957

--- src/main/java/com/team/creer_back/controller/member/MyPageController.java
+++ src/main/java/com/team/creer_back/controller/member/MyPageController.java
@@ -6,6 +6,7 @@ import com.team.creer_back.service.member.MemberService;
 import com.team.creer_back.service.member.MyPageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,16 +31,26 @@ public class MyPageController {
     }
 
     // 정보 수정을 위해서 입력 받은 정보들이 존재하는지 확인
+    // 토큰에 해당된 사용자의 비밀번호와 클라이언트가 전달한 비밀번호 비교
     @PostMapping("/checkInfo")
-    public boolean checkMemberInfo(@RequestBody MemberDto memberDto) {
-        return myPageService.checkMemberInfo(memberDto);
+    public ResponseEntity<Boolean> checkMemberInfo(@RequestHeader("Authorization") String accessToken, @RequestBody MemberDto memberDto) {
+        log.info("memberDto: {}", memberDto.getPassword());
+        return ResponseEntity.ok(myPageService.checkMemberInfo(memberDto.getPassword()));
     }
 
     // 회원 탈퇴
     @DeleteMapping("/delete")
     public ResponseEntity<Boolean> deleteMember(@RequestHeader("X-Email") String email) {
-        boolean isTrue = myPageService.deleteMember(email);
-        return ResponseEntity.ok(isTrue);
+        try {
+            if (myPageService.deleteMember(email)) {
+                log.info("회원 탈퇴 성공");
+                return ResponseEntity.ok(true);
+            } else {
+                return ResponseEntity.badRequest().body(false);
+            }
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(false);
+        }
     }
 
     // 회원 수정
@@ -48,5 +59,55 @@ public class MyPageController {
         log.info("memberDto: {}", memberDto.getEmail());
         boolean isTrue = memberService.modifyMember(memberDto);
         return ResponseEntity.ok(isTrue);
+    }
+
+    @PutMapping("/updateName")
+    public ResponseEntity<Boolean> updateNickname(@RequestHeader("Authorization") String accessToken, @RequestBody MemberReqDto memberDto) {
+        log.info("memberDto: {}", memberDto.getNickName());
+        if (myPageService.updateNickName(memberDto)) {
+            return ResponseEntity.ok(true);
+        }
+        return ResponseEntity.badRequest().body(false);
+    }
+
+    @PutMapping("/updatePw")
+    public ResponseEntity<Boolean> updatePassword(@RequestHeader("Authorization") String accessToken, @RequestBody MemberReqDto memberDto) {
+        log.info("memberDto: {}", memberDto.getPassword());
+        if (myPageService.updatePassword(memberDto)) {
+            return ResponseEntity.ok(true);
+        }
+        return ResponseEntity.badRequest().body(false);
+    }
+
+    @PutMapping("/updatePhone")
+    public ResponseEntity<Boolean> updatePhone(@RequestHeader("Authorization") String accessToken, @RequestBody MemberReqDto memberDto) {
+        log.info("memberDto: {}", memberDto.getPhoneNum());
+        if (myPageService.updatePhone(memberDto)) {
+            return ResponseEntity.ok(true);
+        }
+        return ResponseEntity.badRequest().body(false);
+    }
+
+    @PutMapping("/updateAddress")
+    public ResponseEntity<Boolean> updateAddress(@RequestHeader("Authorization") String accessToken, @RequestBody MemberReqDto memberDto) {
+        log.info("memberDto: {}", memberDto.getAddress());
+        if (myPageService.updateAddress(memberDto)) {
+            return ResponseEntity.ok(true);
+        }
+        return ResponseEntity.badRequest().body(false);
+    }
+
+    @PutMapping("/updateEmail")
+    public ResponseEntity<?> updateEmail(@RequestHeader("Authorization") String accessToken, @RequestBody MemberReqDto memberDto) {
+        log.info("memberDto: {}", memberDto.getEmail());
+        try {
+            if (myPageService.updateEmail(memberDto)) {
+                return ResponseEntity.ok(true);
+            }
+            return ResponseEntity.badRequest().body(false);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("이미 존재하는 이메일입니다.");
+        }
+
     }
 }
--- src/main/java/com/team/creer_back/service/member/MyPageService.java
+++ src/main/java/com/team/creer_back/service/member/MyPageService.java
@@ -1,22 +1,29 @@
 package com.team.creer_back.service.member;
 
 import com.team.creer_back.dto.member.MemberDto;
+import com.team.creer_back.dto.member.MemberReqDto;
 import com.team.creer_back.entity.member.Member;
 import com.team.creer_back.repository.member.MemberRepository;
+import com.team.creer_back.security.SecurityUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.Optional;
 
 @Slf4j
 @Service
+@Transactional(readOnly = true)
 public class MyPageService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Autowired
-    public MyPageService(MemberRepository memberRepository){
+    public MyPageService(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
         this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     // 이미지 등록
@@ -27,24 +34,20 @@ public class MyPageService {
                     .orElseThrow(() -> new IllegalArgumentException("Invalid member ID: " + memberDto.getId()));
             member.setImage(memberDto.getImage());
             return true;
-        }
-        catch (Exception e){ // 롤백 발생
+        } catch (Exception e) { // 롤백 발생
             return false;
         }
     }
 
     // 회원 존재 여부 확인
-    public boolean checkMemberInfo(MemberDto memberDto) {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String encodedPassword = passwordEncoder.encode(memberDto.getPassword());
-        Optional<Member> optionalMember = memberRepository.findByEmail(memberDto.getEmail());
-        // "Optional<Member> member" 와 같이 선언할 시, member가 NULL일수도 있기 때문에, getter 메서드를 사용할 수가 없다.
+    public boolean checkMemberInfo(String password) { // 리액트로부터 받은 문자열 그 자체 '1q2w3e4r'
+        // 현재 로그인한 사용자의 정보를 가져온다.
+        Long memberId = SecurityUtil.getCurrentMemberId();
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
 
         if (optionalMember.isPresent()) { // optionalMember에 값이 존재한다면 true, 없다면 false 반환
             Member member = optionalMember.get();
-            return member.getName().equals(memberDto.getName())
-                    && passwordEncoder.matches(encodedPassword, member.getPassword())
-                    && member.getPhoneNum().equals(memberDto.getPhoneNum());
+            return passwordEncoder.matches(password, member.getPassword()); // 문자열 그 자체(1q2w3e4r)와 암호화된 문자열을 비교
         } else {
             return false;
         }
@@ -62,4 +65,75 @@ public class MyPageService {
         }
     }
 
+    // 닉네임 변경
+    @Transactional
+    public boolean updateNickName(MemberReqDto reqDto) {
+        Long memberId = SecurityUtil.getCurrentMemberId();
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+        // 토큰을 통해 사용자 정보를 찾고 닉네임 중복이 아니면 닉네임 변경
+        if (optionalMember.isPresent() && !memberRepository.existsByNickName(reqDto.getNickName())) {
+            Member member = optionalMember.get();
+            member.setNickName(reqDto.getNickName());
+            return true;
+        }
+        return false;
+    }
+
+    // 비밀번호 변경
+    @Transactional
+    public boolean updatePassword(MemberReqDto reqDto) {
+        Long memberId = SecurityUtil.getCurrentMemberId();
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+        if (optionalMember.isPresent()) {
+            Member member = optionalMember.get();
+
+            member.setPassword(passwordEncoder.encode(reqDto.getPassword()));
+            return true;
+        }
+        return false;
+    }
+
+    // 전화번호 변경
+    @Transactional
+    public boolean updatePhone(MemberReqDto reqDto) {
+        Long memberId = SecurityUtil.getCurrentMemberId();
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+        if (optionalMember.isPresent()) {
+            Member member = optionalMember.get();
+            member.setPhoneNum(reqDto.getPhoneNum());
+            return true;
+        }
+        return false;
+    }
+
+    // 주소 변경
+    @Transactional
+    public boolean updateAddress(MemberReqDto reqDto) {
+        Long memberId = SecurityUtil.getCurrentMemberId();
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+        if (optionalMember.isPresent()) {
+            Member member = optionalMember.get();
+            member.setAddress(reqDto.getAddress());
+            return true;
+        }
+        return false;
+    }
+
+    // 이메일 변경
+    @Transactional
+    public boolean updateEmail(MemberReqDto reqDto) {
+        // 이메일 중복 확인
+        if (memberRepository.existsByEmail(reqDto.getEmail())) {
+            throw new RuntimeException("이미 가입되어 있는 유저입니다");
+        }
+
+        Long memberId = SecurityUtil.getCurrentMemberId();
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+        if (optionalMember.isPresent()) {
+            Member member = optionalMember.get();
+            member.setEmail(reqDto.getEmail());
+            return true;
+        }
+        return false;
+    }
 }
