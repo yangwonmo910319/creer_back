9a5ab2779742a14187e8f1e24b7083f9ffbb86c6
ydj1215 <ydjdj2@naver.com>
2023-12-27 10:17:35 +0900

    231227 1016 ydj

---
+++ src/main/java/com/team/creer_back/repository/chat/ChatRoomRepository.java
@@ -0,0 +1,7 @@
+package com.team.creer_back.repository.chat;
+
+
+import com.team.creer_back.entity.chat.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> { }
